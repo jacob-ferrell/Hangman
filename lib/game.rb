d4fee9509a30cee41df7e49fad097b92e9b222b1
jacob-ferrell <michaeljacobferrell@gmail.com>
2022-05-19 15:30:28 -0400

    added ability to save and load games.  added display of correct/incorrect guesses

--- lib/game.rb
+++ lib/game.rb
@@ -1,13 +1,15 @@
+require 'yaml'
+
 class Game
 
   @@dictionary = File.readlines('google-10000-english-no-swears.txt')
-  def initialize(word=get_word, guesses_remaining=8, letters_used=[[],[]], progress='_'*word.length)
-    
-    @word = word
+  def initialize(save={})  #word=get_word, guesses_remaining=8, letters_used=[[],[]], progress='_'*word.length)
+    @end_game = false
+    @word = save['word'] || get_word
     puts @word
-    @guesses_remaining = guesses_remaining
-    @letters_used = letters_used
-    @progress = progress.chars
+    @guesses_remaining = save['guesses_remaining'] || 8
+    @letters_used = save['letters_used'] || [[],[]]
+    @progress = save['progress'] || ('_' * @word.length).chars
     play_game 
 
   end
@@ -19,25 +21,30 @@ class Game
 
   def play_game
     
-    while !game_over
+    while !game_over && !@end_game
       display
       get_guess
     end
-    play_again
+
+    play_again if !@end_game
   end
 
 
   def display
     
     puts "\n\n#{'-' * 40}\n\nIncorrect guesses remaining: #{@guesses_remaining}"
-    puts "\n          #{@progress.join(' ')}        Letters used: #{@letters_used}"
+    puts "\n          #{@progress.join(' ')}        Correct letters guessed: #{@letters_used[0]}     Incorrect letters guessed: #{@letters_used[1]}"
     
   end
 
   def get_guess
-    puts "\n\nEnter a letter to make your next guess:"
+    puts "\n\nEnter a letter to make your next guess, or press 3 to save:"
     guess = gets.chomp.downcase[0]
-    invalid_guess if (/[a-z]/ =~ guess).nil?
+    if guess == '3'
+      return save_game
+    elsif (/[a-z]/ =~ guess).nil?
+      invalid_guess 
+    end
     @word.include?(guess) ? correct_guess(guess) : incorrect_guess(guess)
   end
 
@@ -49,9 +56,32 @@ class Game
   end
 end
 
+  def incorrect_guess(guess)
+    puts "\nYour guess was incorrect!"
+    @letters_used[1].push(guess)
+    @guesses_remaining -= 1
+  end
+
   def invalid_guess
     puts "\nInvalid guess."
     get_guess
+  end
+
+  def save_game
+    @end_game = true
+    game_data = {
+      'word'=>@word,
+      'guesses_remaining'=>@guesses_remaining,
+      'letters_used'=>@letters_used,
+      'progress'=>@progress
+    }
+    
+    puts "\nEnter a name for your save: "
+    Dir.mkdir('saves') unless Dir.exist?('saves')
+    name = gets.chomp
+    filename = "saves/#{name}.yml"
+    File.open(filename, 'w') { |file| file.write game_data.to_yaml }
+    File.exist?(filename) if puts "\nGame successfully saved!"
   end
 
   def game_over
--- lib/new_or_load.rb
+++ lib/new_or_load.rb
@@ -1,4 +1,5 @@
 require_relative 'game.rb'
+require 'yaml'
 
 class NewOrLoad
 
@@ -12,7 +13,24 @@ class NewOrLoad
       choice == '1' ? Game.new : choice == '2' ? load_save : invalid_choice
     
   end
+
+  def load_save
+    save_name = get_save_name
+    save = YAML.load_file("saves/#{save_name}.yml")
+    Game.new(save)
+  end
   
+  def get_save_name
+    puts "\nEnter the name of the save you wish to load: "
+    save_name = gets.chomp
+    if File.exist?("saves/#{save_name}.yml")
+      save_name
+    else
+      puts "\nThat save file does not exist"
+      get_save_name
+    end   
+  end
+
   def invalid_choice
     puts "\nInvalid Choice.  Enter 1 to play a new game, or 2 to load a save."
     new_or_load
